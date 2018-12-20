0696fe41a7b61d3bd78eb973f1120bd4f7a36c20
kachind <42634028+kachind@users.noreply.github.com>
2018-12-20 13:02:55 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,10 +10,14 @@ c) CPU=${OPTARG};;
 esac
 done
 
+echo "$WALLET"
+echo "$POOL"
 echo "$CPU"
 
 if [ "$CPU" -eq 0 ];then
   CPU=$((`getconf _NPROCESSORS_ONLN`/4))
 fi
+
+echo "$CPU"
 
 UV_THREADPOOL_SIZE=${CPU} ./miner --wallet-address="$WALLET" --pool=${POOL} --miner=${CPU} --in-memory
