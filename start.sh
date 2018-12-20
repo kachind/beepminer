1822534fe392612e804c5f480c96ee1eedd1bb9f
kachind <42634028+kachind@users.noreply.github.com>
2018-12-19 19:41:26 -0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+while getopts w:p:c: option
+do
+case "${option}"
+in
+w) WALLET=${OPTARG};;
+p) POOL=${OPTARG};;
+c) CPU=${OPTARG};;
+esac
+done
+
+UV_THREADPOOL_SIZE=${CPU} ./miner --wallet-address=${WALLET} --pool={POOL} --miner={CPU} --in-memory
