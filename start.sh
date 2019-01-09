59251a70d2a26d42565afd820e05c8e09fbb01ee
kachind <42634028+kachind@users.noreply.github.com>
2019-01-09 10:42:57 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-while getopts :w:p:c: option
+while getopts :w:p:c:s option
 do
 case "${option}"
 in
 w) WALLET=${OPTARG};;
 p) POOL=${OPTARG};;
 c) CPU=${OPTARG};;
+s) SEED=${OPTARG};;
 esac
 done
 
@@ -14,4 +15,4 @@ if [ "$CPU" -eq 0 ];then
   CPU=$((`getconf _NPROCESSORS_ONLN`/4))
 fi
 
-UV_THREADPOOL_SIZE=${CPU} ./miner --wallet-address="$WALLET" --pool=${POOL} --miner=${CPU} --in-memory
+UV_THREADPOOL_SIZE=${CPU} ./miner --wallet-address="$WALLET" --pool=${POOL} --miner=${CPU} --seed=${SEED} --in-memory
